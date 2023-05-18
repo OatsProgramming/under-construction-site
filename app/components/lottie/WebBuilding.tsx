b93316543bd4fd387389657e0e2335e5ab157670
Jack Oats <jackoatmeals@gmail.com>
2023-05-17 21:47:35 -0500

    lower first load

--- app/components/lottie/WebBuilding.tsx
+++ app/components/lottie/WebBuilding.tsx
@@ -1,7 +1,12 @@
 'use client'
 
 import webBuilding from '@/lib/lottie/webBuilding.json'
-import Lottie from 'lottie-react'
+import dynamic from 'next/dynamic'
+
+const Lottie = dynamic(() => 
+    import('lottie-react'),
+    { ssr: false }
+)
 
 export default function WebBuilding() {
     return <Lottie animationData={webBuilding} loop={true}/>
